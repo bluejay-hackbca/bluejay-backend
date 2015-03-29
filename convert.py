30edc1f62ca6de6f6065602227915a873edff932
jinruan <jruan6377@bths.edu>
2015-03-29 07:47:47 -0400

    added summary for key word via wikipedia

--- convert.py
+++ convert.py
@@ -1,16 +1,2 @@
-import re
-from mechanize import Browser
-br = Browser()
-br.set_handle_robots( False )
-br.addheaders = [('User-agent', 'Firefox')]
-
-
-search_term = 'george washington'
-
-br.open("http://www.google.com/")
-br.select_form('f')
-br.form['q'] = search_term
-br.submit()
-#br.follow_link( list(br.links())[0] )
-#print list(br.links())[10]
-#br.follow_link( link )
+import wikipedia
+print wikipedia.summary("george washington", sentences = 2)
