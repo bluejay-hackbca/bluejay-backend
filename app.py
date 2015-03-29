3fade0c96c08a63884182504cf4a229df959a3e4
Genji Noguchi <genji.noguchi@gmail.com>
2015-03-29 04:05:02 -0400

    file separation

--- app.py
+++ app.py
@@ -1,15 +1,23 @@
 from flask import Flask, request
-from convert import TTM
+from convert import TTM, MD_FIXTURE
 
 app = Flask(__name__)
+
 
 @app.route("/submit", methods=['POST'])
 def to_markdown():
     if request.method=="POST":
-        text = request.get("text");
+        #text = request.form['text'];
+        #print "printing:::"
+        #print text
 
         # Manipulate the text here.
-        return TTM(text)
+        #return TTM(text)
+        return MD_FIXTURE
 
 
 
+
+if __name__=="__main__":
+   app.debug=True
+   app.run(host="0.0.0.0",port=5000)
